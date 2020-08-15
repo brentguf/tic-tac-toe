9bf466e6cd84eb664376e742e2c80addcc5e9bf4
brentguf <brent.guffens@gmail.com>
2020-08-15 15:38:52 +0200

    Finish

--- script.rb
+++ script.rb
@@ -18,34 +18,29 @@
 class TicTacToe
   def initialize
     @board = Board.new
-    @player_1 = Player.new('Player 1', 'X')
-    @player_2 = Player.new('Player 2', 'O')
+    @player_1 = Player.new('Player 1', 'X', @board)
+    @player_2 = Player.new('Player 2', 'O', @board)
     @current_player = @player_1
   end
 
   def play
     loop do 
-      token_added = false
-      loop do
-        coords = ask_coordinates
-        valid_coords = validate_coords(coords)
-        if valid_coords do 
-          position_available = @board.check_position(coords)
-          if position_available do 
-            @board.add_token
-            token_added = true
-          else
-            puts "That position has already been taken. Add different coordinates."
-          end
+      @board.render
+      @current_player.play_round
+      
+      if game_over?
+        @board.render
+
+        if @board.winner?(@current_player) 
+          puts "Congrats, #{@current_player.name}. You won!"
         else
-          puts "Invalid input. Please enter the coordinates in the [x,y] format."
+          puts "It\'s a draw!"
         end
 
-        break if token_added
+        break
       end
 
       switch_current_player
-      break if game_over?
     end
   end
 
@@ -53,36 +48,12 @@ class TicTacToe
     @board.winner?(@current_player) || @board.draw?
   end
 
-  # initialize
-    # board
-    # players
-    # current_player
-    # winner?(current_player)
-      # vertical
-      # diagonal
-      # horizontal
-
-  # play
-    # loop
-    # render board
-    # ask_input(current_player) or player.ask_input
-      # check validity
-        # format
-        # board spot is free?
-        # if valid and free => board.place_token
-          # winner?
-            # if yes display message and stop loop
-            # else switch players
-        # else ask input again
-
-  # switch_current_player
+  def switch_current_player
+    @current_player = @current_player === @player_1 ? @player_2 : @player_1
+  end
 end
 
 class Board
-  # initialize
-  # render
-  # place_token
-  # spot = free
   def initialize
     @board = [
       [nil, nil, nil], 
@@ -93,24 +64,28 @@ class Board
 
   def render 
     @board.each_with_index do |row, row_index|
-      to_display = ''
+      row_to_display = ''
     
       row.each_with_index do |square, square_index|
         str = (square || ' ')
         if (square_index != row.length - 1)
           str += ' | '
         end
-        to_display += str
+        row_to_display += str
       end
     
-      puts to_display
-      puts "---------" if row_index != board.length - 1
+      puts row_to_display
+      puts "---------" if row_index != @board.length - 1
     end
   end
 
+  def add_token(coords, piece)
+    @board[coords[0]][coords[1]] = piece
+  end
+
   def winner?(current_player)
-    vertical_winner?(current_player)
-    horizontal_winner?(current_player)
+    vertical_winner?(current_player) ||
+    horizontal_winner?(current_player) ||
     diagonal_winner?(current_player)
   end
 
@@ -126,14 +101,14 @@ class Board
     column_3 = [@board[0][2], @board[1][2], @board[2][2]]
     columns = [column_1, column_2, column_3]
 
-    columns.each do |column|
-      column.each {|square| square == current_player.token}
+    columns.any? do |column|
+      column.all? {|square| square == current_player.token}
     end
   end
 
   def horizontal_winner?(current_player)
-    @board.each do |row|
-      row.each {|square| square == current_player.token}
+    @board.any? do |row|
+      row.all? {|square| square == current_player.token}
     end
   end
 
@@ -142,20 +117,67 @@ class Board
     diagonal_2 = [@board[2][0], @board[1][1], @board[0][2]]
     diagonals = [diagonal_1, diagonal_2]
 
-    diagonals.each do |diagonal|
-      diagonal.each {|diagonal| diagonal == current_player.token}
+    diagonals.any? do |diagonal|
+      diagonal.all? {|diagonal| diagonal == current_player.token}
     end
+  end
+
+  def position_available(coords)
+    @board[coords[0]][coords[1]] == nil
   end
 end
 
 class Player
-  # initialize
-  # check input format validity
-  def initialize(name, token)
+  attr_accessor :token
+
+  def initialize(name, token, board)
     @name = name
     @token = token
+    @board = board
+  end
+
+  def play_round
+    loop do 
+      coords = get_coordinates
+
+      if validate_coords(coords) 
+        position_available = @board.position_available(coords)
+        if position_available 
+          @board.add_token(coords, @token)
+          break
+        else
+          puts "That position has already been taken. Add different coordinates."
+        end
+      end
+    end
+  end
+
+  def get_coordinates
+    puts "#{@name} (#{@token}), enter your coordinates in row,column format (e.g 1,1): "
+    gets.strip.split(",").map{|coord| coord.to_i - 1}
+  end
+
+  def validate_coords(coords)
+    proper_format(coords) &&
+    within_boundaries(coords)
+  end
+
+  def proper_format(coords)
+    if coords.is_a?(Array) && coords.size == 2
+      true
+    else
+      puts "Your coordinates are not in the correct format."
+    end
+  end
+
+  def within_boundaries(coords)
+    if coords.all?{|coord| [0, 1, 2].include?(coord)}
+      true
+    else
+      puts "Your coordinates must be both within the 1-3 range."
+    end
   end
 end
 
 game = TicTacToe.new
-game.start
+game.play
